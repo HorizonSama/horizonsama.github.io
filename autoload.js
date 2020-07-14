4a74a742bb9dc9c5d447732ea4abf6f83d05d922
HorizonSama <414863050@qq.com>
2020-07-14 17:05:43 +0800

    Site updated: 2020-07-14 17:05:43

--- autoload.js
+++ autoload.js
@@ -34,7 +34,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			apiPath: "live2d/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+			cdnPath: "live2d/"
 		});
 	});
 }
